56602bacce194ec434652bc7ce4913c47badac20
admiralmanan <fikricrb001@gmail.com>
2024-12-26 02:18:52 +0700

    Update TubesStrukDat.h

--- TubesStrukDat.h
+++ TubesStrukDat.h
@@ -1,5 +1,5 @@
-#ifndef TUBESSTRUKDAT_H_INCLUDED
-#define TUBESSTRUKDAT_H_INCLUDED
+#ifndef GRAPH_H_INCLUDED
+#define GRAPH_H_INCLUDED
 
 #define idVertex(v) v->idVertex
 #define nextVertex(v) v->nextVertex
@@ -34,10 +34,10 @@ Vertex* findVertex(Graph G, char id);
 void addEdge(Graph &G, char fromID, char toID, int weight);
 void removeEdge(Graph &G, char fromID, char toID);
 void printGraph(Graph G);
-int totalHarga(Graph G, char startVertexID, char endVertexID);
-void DFSRec(Vertex* current, char endVertexID, int currentWeight, int &maxWeight, bool visited[], Graph G);
-void DFS(Graph G, char startVertexID, char endVertexID);
-void DFSTerjauh(Graph G, char startVertexID, char endVertexID);
+void bfs(Graph G, char startVertexID, char endVertexID);
+void dfs(Vertex* current, char endVertexID, int currentWeight, int &maxWeight, bool visited[], bool &foundLongest, Graph G, string path);
 void tampilkanMenu(Graph &G);
+int totalHarga(Graph G, char startVertexID, char endVertexID);
+void dfsTotalHarga(Vertex* current, char endVertexID, int currentWeight, bool visited[], int &totalWeight, Graph G);
 
-#endif // TUBESSTRUKDAT_H_INCLUDED
+#endif // GRAPH_H_INCLUDED
