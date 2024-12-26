8b3936e1d35dce2e01b2a933f52e283907e862de
raatra10 <rafi180350@gmail.com>
2024-12-27 02:05:04 +0700

    Update TubesStrukDat.h

--- TubesStrukDat.h
+++ TubesStrukDat.h
@@ -1,5 +1,5 @@
-#ifndef GRAPH_H_INCLUDED
-#define GRAPH_H_INCLUDED
+#ifndef tGraph_h
+#define tGraph_h
 
 #define idVertex(v) v->idVertex
 #define nextVertex(v) v->nextVertex
@@ -7,37 +7,43 @@
 #define firstVertex(L) L.firstVertex
 
 #include <iostream>
+#include <string>
 using namespace std;
 
-// Struktur data untuk Edge dan Vertex
-struct Edge {
-    char destVertexID;
-    int weight;
-    Edge* nextEdge;
-};
+// Definisi tipe data
+struct Edge;
+struct Vertex;
+
+typedef Vertex* adrVertex;
+typedef Edge* adrEdge;
 
 struct Vertex {
-    char idVertex;
-    Vertex* nextVertex;
-    Edge* firstEdge;
+    string idVertex;
+    adrVertex nextVertex;
+    adrEdge firstEdge;
+};
+
+struct Edge {
+    string destVertexID;
+    int weight;
+    adrEdge nextEdge;
 };
 
 struct Graph {
-    Vertex* firstVertex;
+    adrVertex firstVertex;
 };
 
-// Deklarasi fungsi-fungsi
-void createVertex(char newVertexID, Vertex* &v);
+// Deklarasi fungsi dan prosedur
+void createVertex(string newVertexID, adrVertex &v);
 void initGraph(Graph &G);
-void addVertex(Graph &G, char newVertexID);
-Vertex* findVertex(Graph G, char id);
-void addEdge(Graph &G, char fromID, char toID, int weight);
-void removeEdge(Graph &G, char fromID, char toID);
+void addVertex(Graph &G, string newVertexID);
+adrVertex findVertex(Graph G, string id);
+void addEdge(Graph &G, string fromID, string toID, int weight);
+void removeEdge(Graph &G, string fromVertexID, string toVertexID);
 void printGraph(Graph G);
-void bfs(Graph G, char startVertexID, char endVertexID);
-void dfs(Vertex* current, char endVertexID, int currentWeight, int &maxWeight, bool visited[], bool &foundLongest, Graph G, string path);
+void ruteTerecepat(Graph G, string startVertexID, string endVertexID);
 void tampilkanMenu(Graph &G);
-int totalHarga(Graph G, char startVertexID, char endVertexID);
-void dfsTotalHarga(Vertex* current, char endVertexID, int currentWeight, bool visited[], int &totalWeight, Graph G);
+void tambahRute(Graph &G);
+void tujuan(Graph &G, string &startVertex, string &endVertex);
 
-#endif // GRAPH_H_INCLUDED
+#endif /* tGraph_h */
